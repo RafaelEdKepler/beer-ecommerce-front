44e5cc6d61ced3e97e7ad2a4a8eb821dafde5a6a
Rafael Kepler <rafael.kepler@hotmail.com>
2025-03-24 17:22:37 -0300

    fix: redirect / to /products automatically

--- src/router.jsx
+++ src/router.jsx
@@ -1,10 +1,11 @@
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import ProductListPage from "./pages/product-list-page";
 import ProductDetailsPage from "./pages/product-details-page";
 
 export function Router() {
   return (
     <Routes>
+      <Route path="/" element={<Navigate to="/products" />} />
       <Route path="/products" element={<ProductListPage />} />
       <Route path="/product/:id" element={<ProductDetailsPage />} />
     </Routes>
